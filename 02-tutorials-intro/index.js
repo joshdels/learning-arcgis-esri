844929fa5d7719ba09233aa6f4a7b0f84b7c3daf
joshdels <deleonjoshy2@gmail.com>
2026-05-03 00:01:10 +0800

    learn: basic esri calcite patterns

---
+++ 02-tutorials-intro/index.js
@@ -0,0 +1,59 @@
+const mapEl = document.getElementById("mapElement");
+
+mapEl.addEventListener("arcgisViewReadyChange", (evt) => {
+  const { title, description, thumbnailUrl, avgRating } = mapEl.map.portalItem;
+  document.querySelector("#header-title").heading = title;
+  document.querySelector("#item-description").innerHTML = description;
+  document.querySelector("#item-thumbnail").src = thumbnailUrl;
+  document.querySelector("#item-rating").value = avgRating;
+
+  let activeWidget;
+
+  const handleActionBarClick = ({ target }) => {
+    if (target.tagName !== "CALCITE-ACTION") {
+      return;
+    }
+
+    if (activeWidget) {
+      document.querySelector(`[data-action-id=${activeWidget}]`).active = false;
+      document.querySelector(`[data-panel-id=${activeWidget}]`).closed = true;
+    }
+
+    const nextWidget = target.dataset.actionId;
+    if (nextWidget !== activeWidget) {
+      document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
+      document.querySelector(`[data-panel-id=${nextWidget}]`).closed = false;
+      activeWidget = nextWidget;
+      document.querySelector(`[data-panel-id=${nextWidget}]`).setFocus();
+    } else {
+      activeWidget = null;
+    }
+  };
+
+  // Panel interaction
+  const panelEls = document.querySelectorAll("calcite-panel");
+  for (let i = 0; i < panelEls.length; i++) {
+    panelEls[i].addEventListener("calcitePanelClose", () => {
+      document.querySelector(`[data-action-id=${activeWidget}]`).active = false;
+      document.querySelector(`[data-action-id=${activeWidget}]`).setFocus();
+      activeWidget = null;
+    });
+  }
+
+  document
+    .querySelector("calcite-action-bar")
+    .addEventListener("click", handleActionBarClick);
+
+  let actionBarExpanded = false;
+
+  document.addEventListener("calciteActionBarToggle", (event) => {
+    actionBarExpanded = !actionBarExpanded;
+    const mapElCopyrightText = actionBarExpanded ? "125px" : "45px";
+    mapEl.style.setProperty(
+      "--arcgis-layout-overlay-space-left",
+      `${mapElCopyrightText}`,
+    );
+  });
+
+  document.getElementById("app-loader").hidden = true;
+});
---
+++ README.md
@@ -0,0 +1,18 @@
+# Learning Esri Stack for high quality value
+
+![View](image.png)
+---
+
+## Calcite
+
+1. calcite-shell
+2. calcite-shell-panel
+3. calcite-block
+4. calcite-card
+5. calcite-loader
+---
+
+## ArcGIS SDK
+
+1. arcgis-map
+2. arcgis-zoom
